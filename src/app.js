15f5c9ca11adc4269aaea3fc2be44e081717f3ad
martin-marzer <martinvazquez1104@gmail.com>
2024-05-13 03:38:20 -0300

    visuales de registro e inicio de sesion

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const connectAndSync = async () => {
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
+    //error mas comun: ConnectionRefusedError, no levantaste la bd con xxamp
   }
 };
 
